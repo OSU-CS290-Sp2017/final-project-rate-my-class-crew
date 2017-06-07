7a66ca9eecef74afde92569ae60d81769256b493
Melissa Kiersey <kierseym@flip3.engr.oregonstate.edu>
2017-06-07 14:22:38 -0700

    including handlebars

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ var fs = require('fs');
 var express = require('express');
 var exphbs = require('express-handlebars');
 
+var reviewData = require('./reviews');
 var app = express();
 var port = process.env.PORT || 3000;
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.get('/', function(request, response, next){
+  var templateArgs = {
+    reviews = reviewData
+  };
+  response.render('main', templateArgs);
+});
 app.use(express.static(path.join(__dirname, 'final-project-rate-my-class-crew')));
 app.listen(port, function(){
   console.log("listening on port: ", port);
