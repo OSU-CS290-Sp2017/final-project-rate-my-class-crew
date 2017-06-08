e18cda0b14de0de13a2db00a6056df4e4f42f716
ErinTanaka <tanakae@oregonstate.edu>
2017-06-08 11:34:45 -0700

    changed all instances of twit to review

--- public/index.js
+++ public/index.js
@@ -12,7 +12,7 @@ var classCode =document.getElementById('class-code-input');
 var teacher = document.getElementById('teacher-input');
 var grade = document.getElementById('grade-received-input');
 var starRating;//no clue how to do starRating yet
-var otherText = document.getElementById('twit-text-input');
+var otherText = document.getElementById('review-text-input');
 
 var oneStar=document.getElementById("ratings-1");
 var twoStars=document.getElementById("ratings-2");
@@ -131,138 +131,138 @@ typing.addEventListener('input', function(){
 /*
 // hess's stuff from assignment 5
 //
-// var allTwitElems = [];
+// var allreviewElems = [];
 //
 // /*
-//  * This function shows the modal to create a twit when the "create twit"
+//  * This function shows the modal to create a review when the "create review"
 //  * button is clicked.
 //  */
-// function showCreateTwitModal() {
+// function showCreatereviewModal() {
 //
 //   var modalBackdrop = document.getElementById('modal-backdrop');
-//   var createTwitModal = document.getElementById('create-twit-modal');
+//   var createreviewModal = document.getElementById('create-review-modal');
 //
 //   // Show the modal and its backdrop.
 //   modalBackdrop.classList.remove('hidden');
-//   createTwitModal.classList.remove('hidden');
+//   createreviewModal.classList.remove('hidden');
 //
 // }
 //
 // /*
-//  * This function hides the modal to create a twit and clears any existing
+//  * This function hides the modal to create a review and clears any existing
 //  * values from the input fields whenever any of the modal close actions are
 //  * taken.
 //  */
-// function closeCreateTwitModal() {
+// function closeCreatereviewModal() {
 //
 //   var modalBackdrop = document.getElementById('modal-backdrop');
-//   var createTwitModal = document.getElementById('create-twit-modal');
+//   var createreviewModal = document.getElementById('create-review-modal');
 //
 //   // Hide the modal and its backdrop.
 //   modalBackdrop.classList.add('hidden');
-//   createTwitModal.classList.add('hidden');
+//   createreviewModal.classList.add('hidden');
 //
-//   clearTwitInputValues();
+//   clearreviewInputValues();
 //
 // }
 //
 // /*
-//  * This function clears any value present in any of the twit input elements.
+//  * This function clears any value present in any of the review input elements.
 //  */
-// function clearTwitInputValues() {
+// function clearreviewInputValues() {
 //
-//   var twitInputElems = document.getElementsByClassName('twit-input-element');
-//   for (var i = 0; i < twitInputElems.length; i++) {
-//     var input = twitInputElems[i].querySelector('input, textarea');
+//   var reviewInputElems = document.getElementsByClassName('review-input-element');
+//   for (var i = 0; i < reviewInputElems.length; i++) {
+//     var input = reviewInputElems[i].querySelector('input, textarea');
 //     input.value = '';
 //   }
 //
 // }
 //
 // /*
-//  * Create and return a new HTML element representing a single twit, given the
-//  * twit text and twit attribution as arguments.  The twit element has the
+//  * Create and return a new HTML element representing a single review, given the
+//  * review text and review attribution as arguments.  The review element has the
 //  * following structure:
 //  *
-//  * <article class="twit">
-//  *   <div class="twit-icon">
+//  * <article class="review">
+//  *   <div class="review-icon">
 //  *     <i class="fa fa-bullhorn"></i>
 //  *   </div>
-//  *   <div class="twit-content">
-//  *     <p class="twit-text">
-//  *       {{twitText}}
+//  *   <div class="review-content">
+//  *     <p class="review-text">
+//  *       {{reviewText}}
 //  *     </p>
-//  *     <p class="twit-attribution">
-//  *       <a href="#">{{twitAttribution}}</a>
+//  *     <p class="review-attribution">
+//  *       <a href="#">{{reviewAttribution}}</a>
 //  *     </p>
 //  *   </div>
 //  * </article>
 //  */
-// function generateNewTwitElem(twitText, twitAuthor) {
+// function generateNewreviewElem(reviewText, reviewAuthor) {
 //
-//   var twitTemplate = Handlebars.templates.twit;
-//   var twitData = {
-//     text: twitText,
-//     author: twitAuthor
+//   var reviewTemplate = Handlebars.templates.review;
+//   var reviewData = {
+//     text: reviewText,
+//     author: reviewAuthor
 //   };
 //
-//   return twitTemplate(twitData);
+//   return reviewTemplate(reviewData);
 //
 // }
 //
 // /*
-//  * This function takes user input values from the "create twit" modal,
-//  * generates a new twit using them, and inserts that twit into the document.
+//  * This function takes user input values from the "create review" modal,
+//  * generates a new review using them, and inserts that review into the document.
 //  */
-// function insertNewTwit() {
+// function insertNewreview() {
 //
-//   var twitText = document.getElementById('twit-text-input').value;
-//   var twitAttribution = document.getElementById('twit-attribution-input').value;
+//   var reviewText = document.getElementById('review-text-input').value;
+//   var reviewAttribution = document.getElementById('review-attribution-input').value;
 //
 //   /*
-//    * Only generate the new twit if the user supplied values for both the twit
-//    * text and the twit attribution.  Give them an alert if they didn't.
+//    * Only generate the new review if the user supplied values for both the review
+//    * text and the review attribution.  Give them an alert if they didn't.
 //    */
-//   if (twitText && twitAttribution) {
+//   if (reviewText && reviewAttribution) {
 //
-//       var newTwitElem = generateNewTwitElem(twitText, twitAttribution);
-//       var twitContainer = document.querySelector('.twit-container');
-//       twitContainer.appendChild(newTwitElem);
-//       allTwitElems.push(newTwitElem);
+//       var newreviewElem = generateNewreviewElem(reviewText, reviewAttribution);
+//       var reviewContainer = document.querySelector('.review-container');
+//       reviewContainer.appendChild(newreviewElem);
+//       allreviewElems.push(newreviewElem);
 //
-//       closeCreateTwitModal();
+//       closeCreatereviewModal();
 //
 //   } else {
 //
-//     alert('You must specify both the text and the author of the twit!');
+//     alert('You must specify both the text and the author of the review!');
 //
 //   }
 // }
 //
 // /*
-//  * Perform a search over over all the twits based on the search query the user
-//  * entered in the navbar.  Only display twits that match the search query.
-//  * Display all twits if the search query is empty.
+//  * Perform a search over over all the reviews based on the search query the user
+//  * entered in the navbar.  Only display reviews that match the search query.
+//  * Display all reviews if the search query is empty.
 //  */
-// function doTwitSearch() {
+// function doreviewSearch() {
 //
 //   // Grab the search query, make sure it's not null, and do some preproessing.
 //   var searchQuery = document.getElementById('navbar-search-input').value;
 //   searchQuery = searchQuery ? searchQuery.trim().toLowerCase() : '';
 //
-//   // Remove all twits from the twit container temporarily.
-//   var twitContainer = document.querySelector('.twit-container');
-//   while (twitContainer.lastChild) {
-//     twitContainer.removeChild(twitContainer.lastChild);
+//   // Remove all reviews from the review container temporarily.
+//   var reviewContainer = document.querySelector('.review-container');
+//   while (reviewContainer.lastChild) {
+//     reviewContainer.removeChild(reviewContainer.lastChild);
 //   }
 //
 //   /*
-//    * Loop through the collection of all twits and add twits back into the DOM
+//    * Loop through the collection of all reviews and add reviews back into the DOM
 //    * if they contain the search term or if the search term is empty.
 //    */
-//   allTwitElems.forEach(function (twitElem) {
-//     if (!searchQuery || twitElem.textContent.toLowerCase().indexOf(searchQuery) !== -1) {
-//       twitContainer.appendChild(twitElem);
+//   allreviewElems.forEach(function (reviewElem) {
+//     if (!searchQuery || reviewElem.textContent.toLowerCase().indexOf(searchQuery) !== -1) {
+//       reviewContainer.appendChild(reviewElem);
 //     }
 //   });
 //
@@ -274,29 +274,29 @@ typing.addEventListener('input', function(){
 //  */
 // window.addEventListener('DOMContentLoaded', function () {
 //
-//   // Remember all of the existing twits in an array that we can use for search.
-//   var twitElemsCollection = document.getElementsByClassName('twit');
-//   for (var i = 0; i < twitElemsCollection.length; i++) {
-//     allTwitElems.push(twitElemsCollection[i]);
+//   // Remember all of the existing reviews in an array that we can use for search.
+//   var reviewElemsCollection = document.getElementsByClassName('review');
+//   for (var i = 0; i < reviewElemsCollection.length; i++) {
+//     allreviewElems.push(reviewElemsCollection[i]);
 //   }
 //
-//   var createTwitButton = document.getElementById('create-twit-button');
-//   createTwitButton.addEventListener('click', showCreateTwitModal);
+//   var createreviewButton = document.getElementById('create-review-button');
+//   createreviewButton.addEventListener('click', showCreatereviewModal);
 //
-//   var modalCloseButton = document.querySelector('#create-twit-modal .modal-close-button');
-//   modalCloseButton.addEventListener('click', closeCreateTwitModal);
+//   var modalCloseButton = document.querySelector('#create-review-modal .modal-close-button');
+//   modalCloseButton.addEventListener('click', closeCreatereviewModal);
 //
-//   var modalCancalButton = document.querySelector('#create-twit-modal .modal-cancel-button');
-//   modalCancalButton.addEventListener('click', closeCreateTwitModal);
+//   var modalCancalButton = document.querySelector('#create-review-modal .modal-cancel-button');
+//   modalCancalButton.addEventListener('click', closeCreatereviewModal);
 //
-//   var modalAcceptButton = document.querySelector('#create-twit-modal .modal-accept-button');
-//   modalAcceptButton.addEventListener('click', insertNewTwit);
+//   var modalAcceptButton = document.querySelector('#create-review-modal .modal-accept-button');
+//   modalAcceptButton.addEventListener('click', insertNewreview);
 //
 //   var searchButton = document.getElementById('navbar-search-button');
-//   searchButton.addEventListener('click', doTwitSearch);
+//   searchButton.addEventListener('click', doreviewSearch);
 //
 //   var searchInput = document.getElementById('navbar-search-input');
-//   searchInput.addEventListener('input', doTwitSearch);
+//   searchInput.addEventListener('input', doreviewSearch);
 //
 // });
 
@@ -309,10 +309,10 @@ typing.addEventListener('input', function(){
 
 // //erin's server stuff
 // //buttons
-// var newReviewButton = document.getElementsById('create-twit-button');
+// var newReviewButton = document.getElementsById('create-review-button');
 // //modal variables
 // var modalBackdrop = document.getElementsById('modal-backdrop');
-// var createReviewModal = document.getElementsById('create-twit-modal');
+// var createReviewModal = document.getElementsById('create-review-modal');
 // //fields within modal
 // var classCode = document.getElementsById('class-code-input');
 // var teacher = document.getElementsById('teacher-input');
@@ -334,7 +334,7 @@ typing.addEventListener('input', function(){
 // }
 //
 // function clearModalFields(){
-//   var clearArray = document.getElementsByClassName('twit-input-element');
+//   var clearArray = document.getElementsByClassName('review-input-element');
 //   for(var i=0, i<clearArray.length, i++){
 //     if(clearArray[i].querySelector('input, text')){
 //       var input = clearArray[i].querySelector('input, text');
