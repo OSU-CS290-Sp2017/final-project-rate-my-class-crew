180315534dbfa7b678b4b60b723c00fd93fc1ab4
Melissa Kiersey <kierseym@flip1.engr.oregonstate.edu>
2017-06-10 14:05:14 -0700

    template

--- public/reviewTemplate.js
+++ public/reviewTemplate.js
@@ -3,14 +3,14 @@
 templates['review'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<article class=\"twit\">\r\n        <div class=\"twit-icon\">\r\n          <i class=\"fa fa-pencil\" aria-hidden=\"true\"></i>\r\n        </div>\r\n        <div class=\"twit-content\">\r\n          <p class=\"class-title\">\r\n            <b>Class Code: </b> "
+  return "<article class=\"twit\">\n        <div class=\"twit-icon\">\n          <i class=\"fa fa-pencil\" aria-hidden=\"true\"></i>\n        </div>\n        <div class=\"twit-content\">\n          <p class=\"class-title\">\n            <b>Class Code: </b> "
     + alias4(((helper = (helper = helpers.className || (depth0 != null ? depth0.className : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"className","hash":{},"data":data}) : helper)))
-    + "\r\n          </p>\r\n          <p class=\"class-teacher\">\r\n            <b>Teacher: </b> "
+    + "\n          </p>\n          <p class=\"class-teacher\">\n            <b>Teacher: </b> "
     + alias4(((helper = (helper = helpers.teacher || (depth0 != null ? depth0.teacher : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"teacher","hash":{},"data":data}) : helper)))
-    + "\r\n          </p>\r\n          <p class=\"class-grade\">\r\n            <b>Grade: </b> "
+    + "\n          </p>\n          <p class=\"class-grade\">\n            <b>Grade: </b> "
     + alias4(((helper = (helper = helpers.grade || (depth0 != null ? depth0.grade : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"grade","hash":{},"data":data}) : helper)))
-    + "\r\n          </p>\r\n          <p class=\"class-recommend\">\r\n            <b>Rating: </b>\r\n          </p>\r\n          <p class=\"class-comments\">\r\n            <b>Comments: </b> "
+    + "\n          </p>\n          <p class=\"class-recommend\">\n            <b>Rating: </b>\n          </p>\n          <p class=\"class-comments\">\n            <b>Comments: </b> "
     + alias4(((helper = (helper = helpers.comments || (depth0 != null ? depth0.comments : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"comments","hash":{},"data":data}) : helper)))
-    + "\r\n          </p>\r\n        </div>\r\n      </article>\r\n";
+    + "\n          </p>\n        </div>\n      </article>\n";
 },"useData":true});
 })();
