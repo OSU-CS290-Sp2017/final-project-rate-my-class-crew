c52c572470d42d5fc441c66097e42efd269046b2
ErinTanaka <tanakae@oregonstate.edu>
2017-06-11 12:10:42 -0700

    Stores new review infomation to json file

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ var path = require('path');
 var fs = require('fs');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+
 
 var reviewData = require('./reviews');
 var app = express();
 var port = process.env.PORT || 3000;
 app.engine('handlebars', exphbs({defaultLayout: 'start'}));
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
 
 app.get('/', function(request, response, next){
   response.render('home');
@@ -54,6 +58,53 @@ app.get('/:classID', function(request, response, next){
       next();
     }
 });
+
+//store new post info
+app.post('/:classType/createReview', function (req, res, next) {
+  console.log("sever post function");
+  var review = reviewData[req.params.classType];
+  console.log("made variable");
+  if (review) {
+    console.log("in first conditional");
+    if (req.body) {
+      console.log("in second conditional");
+      // var photo = {
+      //   url: req.body.url,
+      //   caption: req.body.caption
+      // };
+      var temp = {
+        className: req.body.className,
+        teacher: req.body.teacher,
+        grade:req.body.grade,
+        rating:req.body.rating,
+        comments:req.body.comments
+      }
+
+      review.reviews = review.reviews || [];
+
+      review.reviews.push(temp);
+      fs.writeFile('reviews.json', JSON.stringify(reviewData), function (err) {
+        if (err) {
+          res.status(500).send("Unable to save post to \"database\".");
+        } else {
+          res.status(200).send();
+        }
+      });
+
+    }
+    else {
+      res.status(400).send("You're missing a field.");
+    }
+
+  } else {
+    console.log('review variable didnt work next middle ware')
+    next();
+  }
+});
+
+
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
