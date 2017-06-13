cdb2a5d6dc8391afb2ebc6f01c2f5e0da712e466
Umayal Annamalai <annamalu@flip2.engr.oregonstate.edu>
2017-06-13 16:41:35 -0700

    Fixed URL for routing to all reviews

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', function(request, response, next){
   response.render('home');
 });
 
-app.get('/trending', function(request, response, next){
+app.get('/all-reviews', function(request, response, next){
   console.log(reviewData);
   var templateArgs = {
     class: reviewData
